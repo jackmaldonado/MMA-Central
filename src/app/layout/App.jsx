e204a52d9162c5205adc5045b0aa7788df153f80
Jack Maldonado <jackmaldonado@Jacks-MBP.attlocal.net>
2019-09-25 13:01:43 -0700

    added components

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <h1>MMA Cantral</h1>
-
+       <h1>MMA Central</h1>
+        <button className="ui icon button">
+          <i className="smile icon"></i>
+          CSS button
+        </button>
+        <Button icon="smile" content="React Button"></Button>
        </div>
      );
   }
