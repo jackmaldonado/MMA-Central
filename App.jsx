e24253438fc5012415adacde277f287ea374b52b
jackmaldonado <maldonado_jack@yahoo.com>
2019-10-07 14:05:39 -0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import React, { Component, Fragment } from 'react';
+import EventDashboard from '../../features/event/eventDashboard/EventDashboard';
+import NavBar from '../../features/nav/navBar/NavBar';
+import { Container } from 'semantic-ui-react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <NavBar />   
+        <Container className="main">  
+          <EventDashboard />
+        </Container>
+      </Fragment>
+     );
+  }
+}
+
+export default App;
